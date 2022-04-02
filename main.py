b4341dd2de04d0d49f67329b0d5db42443eb417e
Dihia Idri <dihia.idrici@mail.mcgill.ca>
2022-04-02 12:48:46 -0400

    If you input the wrong pixel height or width on your terminal, you get a chance to fix it.

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import numpy as np
 
 def main(H, W, hp, wp):
     # Need the pixel dimensions to be an integer divisor of the poster dimension
-    # For H dimension
     poster_D = [H, W]
     pixel_D = [hp, wp]
-    #Column for H & W, Rows for min and max
+    # Column for H & W, Rows for min and max
     final_D = np.zeros((2, 2))
     final_num = np.zeros((2, 2))
 
@@ -29,30 +28,33 @@ def main(H, W, hp, wp):
     print("Number of pixels for each dimension")
     print(final_num)
     satisfied = 'No'
-    while (satisfied == 'No'):
+    while satisfied == 'No' or satisfied == 'no':
         h_pixel = int(input("Enter the height of the pixel i.e.first column of pixel dimension: "))
         w_pixel = int(input("Enter the width of the pixel i.e.second  column of pixel dimension: "))
-        im = poster(poster_D, h_pixel, w_pixel)
-        im.show()
-        satisfied = input("Are you satisfied? Yes or No?: ")
-        if (satisfied == 'Yes'):
-            im.save('/Users/dihiaidrici/Desktop/SecondPaper/PixelFigure.tif')
+        if (h_pixel == final_D[0][0] or h_pixel == final_D[1][0]) and (w_pixel == final_D[0][1] or w_pixel == final_D[1][1]):
+            im = poster(poster_D, h_pixel, w_pixel)
+            im.show()
+            satisfied = input("Are you satisfied? Yes or No?: ")
+            if satisfied == 'Yes' or satisfied =='yes':
+                im.save('/Users/dihiaidrici/Desktop/SecondPaper/PixelFigure.tif')
+        else:
+            print("You made a mistake selecting the height or width of the pixels. Try again ")
 
 def poster(poster_D, h_pixel, w_pixel):
     h_pixel_num = int(poster_D[0] / h_pixel)
     w_pixel_num = int(poster_D[1] / w_pixel)
-
     # produce random pixel image. 255 is for an 8bit image. for 16bit it would be more
     imarray = np.random.rand(h_pixel_num, w_pixel_num, 3) * 255
     newImage = Image.fromarray(imarray.astype('uint8'))  # .convert('RGBA')
     return newImage
 
+
 def min_max(poster_i, p_i):
-    for p_min in range(p_i,0,-1):
-        if(poster_i % p_min == 0):
+    for p_min in range(p_i, 0, -1):
+        if poster_i % p_min == 0:
             break
     for p_max in range(p_i, poster_i+1, 1):
-        if (poster_i % p_max == 0):
+        if poster_i % p_max == 0:
             break
     return p_min, p_max
 
